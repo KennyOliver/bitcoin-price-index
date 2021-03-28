94769a8440e702a0cf59f163278c508e0e7345ed
Kenny <kenny_oliver@icloud.com>
2021-03-28 18:30:18 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let bitcoin_data = document.getElementById("bitcoin-data");
+
+function getBPI() {
+  //let url = "https://datahub.io/core/population/datapackage.json";
+  //let url = "https://api.coindesk.com/v1/bpi/currentprice.json";
+  let url = "https://api.coinbase.com/v2/prices/spot?currency=USD";
+  
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      let formatted_data = `1 ${data.data.base} = ${data.data.amount} ${data.data.currency}`;
+      console.log(formatted_data);
+      bitcoin_data.innerHTML = formatted_data;
+    });
+}
+
+getBPI();
+setInterval(getBPI,10000);
